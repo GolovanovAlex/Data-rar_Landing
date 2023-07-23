f17bdcc9e0dc163127ab0be13a08458d76bce546
Alex Golovanov <Golovanov.a.a@yandex.ru>
2023-07-23 17:46:31 +0300

    deploy: v0.5

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,11 @@
 let swiperWork = new Swiper('.workSwiper', {
-  slidesPerView: 6,
-  spaceBetween: 20,
   freeMode: true,
   autoHeight: true,
   loop: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  centerMode: false,
+
   navigation: {
     nextEl: '.swiper-but-next',
     prevEl: '.swiper-but-prev',
@@ -12,10 +14,28 @@ let swiperWork = new Swiper('.workSwiper', {
     el: '.swiper-pagination',
     clickable: true,
   },
+  breakpoints: {
+    300: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    500: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 6,
+      spaceBetween: 20,
+    },
+  },
 });
 
 let swiperReview = new Swiper('.reviewSwiper', {
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 20,
   freeMode: true,
   autoHeight: false,
@@ -27,5 +47,16 @@ let swiperReview = new Swiper('.reviewSwiper', {
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
+  },
+  breakpoints: {
+    300: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
   },
 });
