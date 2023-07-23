298a876e8277a093e9f55a08a58e965b34f78f5d
Alex Golovanov <Golovanov.a.a@yandex.ru>
2023-07-23 15:28:01 +0300

    style: finish bg

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,29 @@
-var swiper = new Swiper('.mySwiper', {
-  slidesPerView: 3,
-  spaceBetween: 30,
+let swiperWork = new Swiper('.workSwiper', {
+  slidesPerView: 6,
+  spaceBetween: 20,
   freeMode: true,
+  autoHeight: true,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-but-next',
+    prevEl: '.swiper-but-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+let swiperReview = new Swiper('.reviewSwiper', {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  freeMode: true,
+  autoHeight: false,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-but-next',
+    prevEl: '.swiper-but-prev',
+  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
